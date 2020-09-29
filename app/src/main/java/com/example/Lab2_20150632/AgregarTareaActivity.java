45fa8077eca7f271d299bd5f6285bb9f0c4ff4d6
Alvaro Burga <a20150632@pucp.pe>
2020-09-29 12:21:16 -0500

    lista

--- app/src/main/java/com/example/Lab2_20150632/AgregarTareaActivity.java
+++ app/src/main/java/com/example/Lab2_20150632/AgregarTareaActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class AgregarTareaActivity extends AppCompatActivity {
 
@@ -29,6 +33,28 @@ public class AgregarTareaActivity extends AppCompatActivity {
         {
             this.setTheme(R.style.Mecatronica);
         }
+
+        Button agregar = findViewById(R.id.button2);
+        agregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                EditText tarea = findViewById(R.id.nuevaTarea);
+                String aux=tarea.getText().toString();
+                if(aux.isEmpty())
+                {
+                    tarea.setError("No puede estar vacio");
+                }
+                else
+                {
+                    intent.putExtra("tarea", aux);
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }
+
+            }
+        });
+
 
 
     }
